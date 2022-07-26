6efb31efc37c31b64d49d82cc5d69680cdcd101e
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-07-26 09:29:20 +0100

    time task commit

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,17 +1,32 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Operation {
     //lock method
     //deadlock
     //Atomic -- useful in collections
+    ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     synchronized void multiply(int number){
-        try{
-            for (int i = 0; i<20; i++){
-                System.out.println("Current value after "+Thread.
-                        currentThread().getName()+" "+number*i );
-                Thread.sleep(1000);
-            }
+        try {
+//            for (int i = 0; i<20; i++){
+//                System.out.println("Current value after "+Thread.
+//                        currentThread().getName()+" "+number*i );
+//                Thread.sleep(1000);
+//            }
+            scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    {
+                        System.out.println("Current value after "+Thread.currentThread().getName());
+                    }
+                }
+                                                             };
+        0,1, TimeUnit.SECONDS);
         }
         catch (Exception e){
             System.out.println(e);
         }
     }
+
 }
